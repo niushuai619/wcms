53d6cac9cf10003e174dfc8c7937e6b6354fd155
unknown <shuaishuai.niu@.emagroup.cn>
2015-06-25 12:38:28 +0800

    ceate

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'wcms',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'wm_',
+    //密钥
+    "AUTHCODE" => 'xUZmdL09hEbgVAkZFT',
+    //cookies
+    "COOKIE_PREFIX" => 'pfSdk3_',
+);
+?>
---
+++ data/runtime/Data/site_options.php
@@ -0,0 +1,1 @@
+a:10:{s:9:"site_name";s:18:"游晴网络科技";s:9:"site_host";s:20:"http://www.wcms.com/";s:9:"site_root";s:0:"";s:8:"site_icp";s:0:"";s:16:"site_admin_email";s:16:"396057259@qq.com";s:11:"site_tongji";s:0:"";s:14:"site_copyright";s:0:"";s:14:"site_seo_title";s:18:"游晴网络科技";s:17:"site_seo_keywords";s:0:"";s:20:"site_seo_description";s:6:"游晴";}
