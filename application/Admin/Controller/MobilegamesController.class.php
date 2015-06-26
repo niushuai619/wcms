44fcc9f8172371d9919ec9b45c19d89cdc4a8e6d
unknown <shuaishuai.niu@.emagroup.cn>
2015-06-26 16:38:47 +0800

    sss

--- application/Admin/Controller/MobilegamesController.class.php
+++ application/Admin/Controller/MobilegamesController.class.php
@@ -1,10 +1,10 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// | 手机游戏模块
 // +----------------------------------------------------------------------
-// | Copyright (c) 2013-2014 http://www.thinkcmf.com All rights reserved.
+// | 
 // +----------------------------------------------------------------------
-// | Author: Tuolaji <479923197@qq.com>
+// | Author: niushuaishuai <396057259@qq.com>
 // +----------------------------------------------------------------------
 namespace Admin\Controller;
 use Common\Controller\AdminbaseController;
@@ -14,12 +14,45 @@ class MobilegamesController extends AdminbaseController {
 	
 	function _initialize() {
 		parent::_initialize();
-
+        $this->games_model = M('mobilegames');
 	}
 	function index(){
 		$this->_lists();
-		$this->_getTree();
 		$this->display();
 	}
-	
+    //游戏编辑添加操作
+	public function  handle(){
+	    
+	}
+    //排序
+    public function listorders() {
+        $status = parent::_listorders($this->games_model);
+        if ($status) {
+            $this->success("排序更新成功！");
+        } else {
+            $this->error("排序更新失败！");
+        }
+    }
+    //游戏删除操作
+    public  function delete(){
+        
+    }
+    private function _lists(){
+            
+            
+    }
+    //游戏类型
+     private function game_type(){
+        
+        $type_arr = array(
+                   '1'=>'角色',
+                   '2'=>'卡牌',
+                   '3'=>'竞技',
+                   '4'=>'MOBA',
+                   '5'=>'休闲',
+                   '6'=>'养成',
+                   '7'=>'策略'
+        );
+        return  $type_arr;
+    }
 }
